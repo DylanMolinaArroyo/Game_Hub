b39ce1714f727341985cf1de6513f23f54d2b61a
DylanMA1 <dymolina@estudiantec.cr>
2024-08-10 20:54:10 -0600

    Mas cambios

--- proyectods1/src/components/CriticScoreExpand.tsx
+++ proyectods1/src/components/CriticScoreExpand.tsx
@@ -1,18 +1,9 @@
-import {
-  CircularProgress,
-  CircularProgressLabel,
-  Icon,
-  Stat,
-  StatLabel,
-  StatNumber,
-} from "@chakra-ui/react";
+import { Icon, Stat, StatLabel, StatNumber } from "@chakra-ui/react";
 import { SiMetacritic } from "react-icons/si";
 interface Props {
   score: number;
 }
 const CriticScoreExpand = ({ score }: Props) => {
-  let color = score > 75 ? "green" : score > 60 ? "darkyellow" : "";
-
   return (
     <Stat alignItems="center">
       <StatLabel fontSize={20}>Metacritic Score</StatLabel>
