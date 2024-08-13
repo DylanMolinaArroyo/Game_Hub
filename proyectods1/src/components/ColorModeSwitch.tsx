85cfb2dcd0796e78918d1ab126ba49d9484cb184
DylanMA1 <dymolina@estudiantec.cr>
2024-08-13 14:36:13 -0600

    Boton de tema mejorado

--- proyectods1/src/components/ColorModeSwitch.tsx
+++ proyectods1/src/components/ColorModeSwitch.tsx
@@ -1,11 +1,23 @@
-import { HStack, Switch, Text, useColorMode } from "@chakra-ui/react";
+import { HStack, Switch, Icon, useColorMode } from "@chakra-ui/react";
+import { FaMoon } from "react-icons/fa";
+import { IoSunnySharp } from "react-icons/io5";
 
 const ColorModeSwitch = () => {
   const { toggleColorMode, colorMode } = useColorMode();
+
   return (
-    <HStack>
-      <Switch isChecked={colorMode === "dark"} onChange={toggleColorMode} />
-      <Text whiteSpace="nowrap">Dark Mode</Text>
+    <HStack spacing={4}>
+      <Switch
+        isChecked={colorMode === "dark"}
+        onChange={toggleColorMode}
+        size="lg"
+        variant={colorMode === "dark" ? "moonMode" : "sunMode"}
+      />
+      {colorMode === "dark" ? (
+        <Icon as={FaMoon} boxSize="1.5em" color="blue.600" />
+      ) : (
+        <Icon as={IoSunnySharp} boxSize="1.5em" color="yellow.500" />
+      )}
     </HStack>
   );
 };
